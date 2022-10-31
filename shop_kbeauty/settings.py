525392cebb074d805d15aa37d93fa517b4f8f6bf
JoyZadan <joyzadan.learning@gmail.com>
2022-10-31 19:57:32 +0000

    Complete homepage header and css

--- shop_kbeauty/settings.py
+++ shop_kbeauty/settings.py
@@ -158,4 +158,10 @@ STATIC_URL = '/static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
