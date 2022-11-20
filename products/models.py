82c7429fe6b47f9c0f995970838ca795954fd65f
JoyZadan <joyzadan.learning@gmail.com>
2022-11-20 03:25:47 +0000

    Add: main_category, category, subcategory, brand and product models and resolve #11, #12, #13, #14, and #15

--- products/models.py
+++ products/models.py
@@ -1,3 +1,158 @@
 from django.db import models
 
-# Create your models here.
+
+class Main_Category(models.Model):
+    """ Main Category model """
+    name = models. CharField(max_length=100, null=False, unique=True,
+                             blank=False, verbose_name='Main Category title',
+                             help_text='format: required, max_length=150')
+    slug = models.SlugField(max_length=150, null=False, unique=True,
+                            blank=False, verbose_name='Main Category slug',
+                            help_text='format: required, max_length=150')
+
+    class Meta:
+        """ Meta class for Main Category model """
+        verbose_name = 'Main Category'
+        verbose_name_plural = 'Main Categories'
+        ordering = ['name']
+
+    def __str__(self):
+        """ String representation of Main Category model """
+        return self.name
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+
+class Category(models.Model):
+    """ Category model """
+    main_category = models.ForeignKey(Main_Category, on_delete=models.CASCADE,
+                                      verbose_name='main category title')
+    name = models. CharField(max_length=100, null=False, unique=True,
+                             blank=False, verbose_name='Category title',
+                             help_text='format: required, max_length=150')
+    slug = models.SlugField(max_length=150, null=False, unique=True,
+                            blank=False, verbose_name='Category slug',
+                            help_text='format: required, max_length=250')
+
+    class Meta:
+        """ Meta class for Category model """
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+        ordering = ['name']
+
+    def __str__(self):
+        """ String representation of Category model """
+        return self.name + " -- " + self.main_category.name
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+
+class Subcategory(models.Model):
+    """ Subcategory model """
+    category = models.ForeignKey(Category, on_delete=models.CASCADE,
+                                 verbose_name='category title')
+    name = models. CharField(max_length=100, null=False, unique=True,
+                             blank=False, verbose_name='Subcategory title',
+                             help_text='format: required, max_length=100')
+    slug = models.SlugField(max_length=250, null=False, unique=True,
+                            blank=False, verbose_name='Subcategory slug',
+                            help_text='format: required, max_length=250')
+
+    class Meta:
+        """ Meta class for Subcategory model """
+        verbose_name = 'Subcategory'
+        verbose_name_plural = 'Subcategories'
+        ordering = ['name']
+
+    def __str__(self):
+        """ String representation of Category model """
+        return self.name
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+
+class Brand(models.Model):
+    """ Brand model """
+    name = models. CharField(max_length=100, null=False, unique=True,
+                             blank=False, verbose_name='Brand name',
+                             help_text='format: required, max_length=100')
+    slug = models.SlugField(max_length=100, null=False, unique=True,
+                            blank=False, verbose_name='Brand slug',
+                            help_text='format: required, max_length=150')
+    description = models.TextField(max_length=500, null=False, blank=False,
+                                   verbose_name='Brand description',
+                                   help_text='format: reqd, max_length=500')
+    is_featured = models.BooleanField(default=False)
+
+    class Meta:
+        """ Meta class for Brand model """
+        verbose_name = 'Brand'
+        verbose_name_plural = 'Brands'
+        ordering = ['name']
+
+    def __str__(self):
+        """ String representation of Product model """
+        return self.name
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+
+class Product(models.Model):
+    """ Product model """
+    category = models.ForeignKey('Category', on_delete=models.CASCADE,
+                                 verbose_name='category title')
+    brand = models. ForeignKey('Brand', on_delete=models.CASCADE,
+                               related_name='products',
+                               verbose_name='brand title')
+    sku = models.CharField(max_length=50, null=False, unique=False,
+                           blank=False,
+                           help_text='format: required,max_length=50')
+    name = models. CharField(max_length=200, null=False, unique=True,
+                             blank=False, verbose_name='Product name',
+                             help_text='format: required, max_length=200')
+    slug = models.SlugField(max_length=250, null=False, unique=True,
+                            blank=False, verbose_name='Product slug',
+                            help_text='format: required, max_length=250')
+    is_featured = models.BooleanField(default=False)
+    total_quantity = models.IntegerField(null=False, unique=True, blank=False,
+                                         verbose_name='Product quantity')
+    availability = models.IntegerField(null=False, unique=True, blank=False)
+    description = models.TextField(max_length=500, null=False, blank=False,
+                                   verbose_name='Product description',
+                                   help_text='format: required, max_length=50')
+    how_to_use = models.TextField(max_length=500, null=False, blank=False,
+                                  verbose_name='How to use',
+                                  help_text='format: required, max_length=500')
+    ingredients = models.TextField(max_length=500, null=False, blank=False,
+                                   verbose_name='Product ingredients',
+                                   help_text='format: reqd, max_length=500')
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    discount = models.DecimalField(max_digits=6, decimal_places=2)
+    image_url = models.URLField(max_length=1500, null=False, blank=False)
+    image = models.ImageField(null=False, blank=False,)
+
+    class Meta:
+        """ Meta class for Product model """
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
+        ordering = ['name']
+
+    def __str__(self):
+        """ String representation of Product model """
+        return self.name
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    def get_tags(self):
+        """ Get all available tags of products """
+        return self.tags.all()
