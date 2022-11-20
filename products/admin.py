28fe458c6c63771881994f1b9ee6abff77f07603
JoyZadan <joyzadan.learning@gmail.com>
2022-11-20 04:32:19 +0000

    Add: main_category, category, subcategory to django admin, add products details, WIP

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Main_Category, Category, Subcategory, Brand, Product
 
 # Register your models here.
+
+admin.site.register(Main_Category)
+admin.site.register(Category)
+admin.site.register(Subcategory)
+admin.site.register(Brand)
+admin.site.register(Product)
--- products/models.py
+++ products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.text import slugify
 
 
 class Main_Category(models.Model):
@@ -136,7 +137,7 @@ class Product(models.Model):
                                    help_text='format: reqd, max_length=500')
     price = models.DecimalField(max_digits=6, decimal_places=2)
     discount = models.DecimalField(max_digits=6, decimal_places=2)
-    image_url = models.URLField(max_length=1500, null=False, blank=False)
+    image_url = models.URLField(max_length=1500, null=True, blank=True)
     image = models.ImageField(null=False, blank=False,)
 
     class Meta:
