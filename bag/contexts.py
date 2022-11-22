4a24aa3f79b0edbc47ddfa47f35eb023c92bd4a1
JoyZadan <joyzadan.learning@gmail.com>
2022-11-22 18:26:56 +0000

    Create bag app and added add to bag functionality

--- bag/contexts.py
+++ bag/contexts.py
@@ -10,6 +10,7 @@ def bag_contents(request):
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
+        # lets shoppers know how much they need to spend to get free delivery
         free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
     else:
         delivery = 0
