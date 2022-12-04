a9b8a09e425df348aeb1fd030abc792c792a62df
JoyZadan <joyzadan.learning@gmail.com>
2022-12-04 20:32:47 +0000

    Added checkout views and templates, adjusted product detail page margin and deleted commented out code on bag page

--- shop_kbeauty/settings.py
+++ shop_kbeauty/settings.py
@@ -50,6 +50,9 @@ INSTALLED_APPS = [
     'products',
     'bag',
     'checkout',
+
+    # Other
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -63,6 +66,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'shop_kbeauty.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -78,8 +83,13 @@ TEMPLATES = [
                 'django.template.context_processors.request',  # allauth req
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
