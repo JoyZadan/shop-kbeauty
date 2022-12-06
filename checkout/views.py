648a1a6e7ae5eb8321416e125d7dd15a52e237fa
JoyZadan <joyzadan.learning@gmail.com>
2022-12-06 22:55:47 +0000

    Refactor: fix pycodestyle error showing for checkout views line too long

--- checkout/views.py
+++ checkout/views.py
@@ -68,7 +68,8 @@ def checkout(request):
                         )
                         order_line_item.save()
                     else:
-                        for size, quantity in item_data['items_by_size'].items():
+                        for size, \
+                                quantity in item_data['items_by_size'].items():
                             order_line_item = OrderLineItem(
                                 order=order,
                                 product=product,
