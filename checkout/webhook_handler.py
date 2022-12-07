4bc7620fcc7eba8ce284f708ab74b09e0a016477
JoyZadan <joyzadan.learning@gmail.com>
2022-12-07 01:51:02 +0000

    Completed adding order confirmation emails and resolve #45

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -105,6 +105,7 @@ class StripeWH_Handler:
                 attempt += 1
                 time.sleep(1)
         if order_exists:
+            self._send_confirmation_email(order)
             return HttpResponse(
                 content=f'Webhook received: {event["type"]} | \
                     SUCCESS: Verified order already in database',
@@ -151,6 +152,7 @@ class StripeWH_Handler:
                 return HttpResponse(
                     content=f'Webhook received: {event["type"]} | ERROR: {e}',
                     status=500)
+        self._send_confirmation_email(order)
         return HttpResponse(
             content=f'Webhook received: {event["type"]} | \
                 SUCCESS: Created order in webhook',
