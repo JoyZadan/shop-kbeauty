0c46cc9ba7e49e7f1b99ffd252172fad0ed47f88
JoyZadan <joyzadan.learning@gmail.com>
2022-12-08 14:21:31 +0000

    Completed setup for reviews app, views, urls, rendered reviews and add review, and resolve #19

--- reviews/urls.py
+++ reviews/urls.py
@@ -3,4 +3,5 @@ from .import views
 
 urlpatterns = [
     path('reviews/<int:product_id>', views.reviews, name='reviews'),
+    path('add_review/<int:product_id>', views.add_review, name='add_review'),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.contrib import messages
 from products.models import Product
 from .models import Review
 from .forms import ReviewForm
+from profiles.models import UserProfile
 
 
 def reviews(request, product_id):
@@ -14,6 +15,35 @@ def reviews(request, product_id):
 
     context = {
         'reviews': reviews,
+    }
+
+    return render(request, template, context)
+
+
+def add_review(request, product_id):
+    """ Renders a form to allow user to add a review """
+    user = UserProfile.objects.get(user=request.user)
+    product = Product.objects.get(id=product_id)
+    if request.method == "POST":
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            review = form.save()
+            review.product = product
+            review.user = request.user
+            review.save()
+            messages.success(request, "New product review added.")
+            return redirect(reverse('review_detail', args=[review.id]))
+        else:
+            messages.error(request, "Form invalid, please try again.")
+            return redirect(reversed('product_details', args=[product.id]))
+    else:
+        form = ReviewForm()
+
+    template = 'reviews/add_review.html'
+
+    context = {
+        'form': form,
+        'product': product,
     }
 
     return render(request, template, context)
