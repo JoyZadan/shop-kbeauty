895a4bb989cbaf02497321336af107fb1fa0130f
JoyZadan <joyzadan.learning@gmail.com>
2022-12-09 22:25:10 +0000

    Second attempt to fix Heroku build fail

--- shop_kbeauty/settings.py
+++ shop_kbeauty/settings.py
@@ -182,13 +182,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
-
-STATIC_URL = '/static/'
-# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -228,5 +222,4 @@ DEFAULT_FROM_EMAIL = 'shopkbeauty@example.com'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
