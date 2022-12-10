2024ebee6fd55833cf811ea787dd7c3bab014828
JoyZadan <joyzadan.learning@gmail.com>
2022-12-10 14:30:52 +0000

    Change settings file

--- shop_kbeauty/settings.py
+++ shop_kbeauty/settings.py
@@ -200,6 +200,7 @@ if 'USE_AWS' in os.environ:
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+    AWS_DEFAULT_ACL = 'public-read'
 
     # Static and media files
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
