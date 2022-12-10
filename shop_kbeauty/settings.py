8265127f224dfe6f341e958f1949482c7c70c5c5
JoyZadan <joyzadan.learning@gmail.com>
2022-12-10 17:37:45 +0000

    Added cache control to AWS3 config in settings file

--- shop_kbeauty/settings.py
+++ shop_kbeauty/settings.py
@@ -194,6 +194,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # To connect Django to S3, tell it w/c bucket it shld be communicating w/
 # Use AWS S3 bucket if USE_AWS in os.environ
 if 'USE_AWS' in os.environ:
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'shop-k-beauty'
     AWS_S3_REGION_NAME = 'eu-west-2'
