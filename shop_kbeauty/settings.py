bad91cdcf12c633898bdf820256b781e30ef2661
JoyZadan <joyzadan.learning@gmail.com>
2022-12-10 13:51:10 +0000

    Change settings file

--- shop_kbeauty/settings.py
+++ shop_kbeauty/settings.py
@@ -186,7 +186,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
