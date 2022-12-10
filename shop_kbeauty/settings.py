8c27389b33d8c8485281d1f9a1da00c1705dbda4
JoyZadan <joyzadan.learning@gmail.com>
2022-12-10 15:51:21 +0000

    Changes to settings in prep for a new bucket

--- shop_kbeauty/settings.py
+++ shop_kbeauty/settings.py
@@ -193,24 +193,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # To connect Django to S3, tell it w/c bucket it shld be communicating w/,
 # Check if there's an env variable called USE_AWS in the env
-if 'USE_AWS' in os.environ:
-    # Bucket config
-    AWS_STORAGE_BUCKET_NAME = 'shop-k-beauty-django-joy-zadan'
-    AWS_S3_REGION_NAME = 'eu-west-2'
-    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
-    AWS_DEFAULT_ACL = 'public-read'
-
-    # Static and media files
-    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
-    STATICFILES_LOCATION = 'static'
-    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
-    MEDIAFILES_LOCATION = 'media'
-
-    # Override static and media URLs in production
-    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
-    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 
 
 # auto calculating free delivery
