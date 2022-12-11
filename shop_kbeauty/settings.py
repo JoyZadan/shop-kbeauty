8daa2b2b41cd61ea6e2e32214f22132dfeb145d8
JoyZadan <joyzadan.learning@gmail.com>
2022-12-11 19:48:46 +0000

    Refactor: added shopping bag desktop/mobile view quantity bug fix by replacing id with class

--- shop_kbeauty/settings.py
+++ shop_kbeauty/settings.py
@@ -31,6 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
+# DEBUG = True
 
 ALLOWED_HOSTS = ['shop-k-beauty-django-joy-zadan.herokuapp.com', 'localhost']
 
