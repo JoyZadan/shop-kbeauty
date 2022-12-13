f84707a3df406b7f02636991ae23343435740bf4
JoyZadan <joyzadan.learning@gmail.com>
2022-12-13 10:21:07 +0000

    Edited password reset key message, added padding to allauth templates, tested password recovery and resolve #51

--- shop_kbeauty/settings.py
+++ shop_kbeauty/settings.py
@@ -141,7 +141,6 @@ else:
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-            # 'NAME': BASE_DIR / 'dbsqlite3',
         }
     }
 
