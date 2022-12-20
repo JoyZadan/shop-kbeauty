becc4dc8916e3b161154caa01ed59414cc92f979
JoyZadan <joyzadan.learning@gmail.com>
2022-12-20 00:14:15 +0000

    Added all policy pages with content and links from footer, WIP

--- home/urls.py
+++ home/urls.py
@@ -5,4 +5,9 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home'),
     path('privacy_policy/', views.privacy_policy, name='privacy_policy'),
+    path('terms_and_conditions/', views.terms_and_conditions,
+         name='terms_and_conditions'),
+    path('return_and_refund_policy/', views.return_and_refund_policy,
+         name='return_and_refund_policy'),
+    path('shipping_policy/', views.shipping_policy, name='shipping_policy'),
 ]
--- home/views.py
+++ home/views.py
@@ -18,3 +18,21 @@ def privacy_policy(request):
     """ A view to render the privacy policy page """
 
     return render(request, 'home/privacy_policy.html')
+
+
+def terms_and_conditions(request):
+    """ A view to render the terms and conditions page """
+
+    return render(request, 'home/terms_and_conditions.html')
+
+
+def return_and_refund_policy(request):
+    """ A view to render the return and refund policy page """
+
+    return render(request, 'home/return_and_refund_policy.html')
+
+
+def shipping_policy(request):
+    """ A view to render the shipping policy page """
+
+    return render(request, 'home/shipping_policy.html')
