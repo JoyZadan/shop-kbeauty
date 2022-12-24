a311fba9483833597df1866dca7c636fe865ae99
JoyZadan <joyzadan.learning@gmail.com>
2022-12-24 13:19:54 +0000

    Refactor: added fix to html validation error of duplicate id in bag page

--- shop_kbeauty/settings.py
+++ shop_kbeauty/settings.py
@@ -30,8 +30,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
-# DEBUG = True
+# DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = ['shop-k-beauty-django-joy-zadan.herokuapp.com', 'localhost']
 
