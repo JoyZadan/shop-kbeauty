7ad37f55eacc378a38716f6d713522dd8f082758
JoyZadan <joyzadan.learning@gmail.com>
2023-01-09 23:42:01 +0000

    Created wishlist views, added to project level urls, created wishlist.html and added link to base.html

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
-# Create your views here.
+from profiles.models import UserProfile
+from products.models import Product
+from wishlist.models import Wishlist
+
+
+@login_required
+def wishlist(request):
+    """ A view to show the user's wishlist """
+    user = get_object_or_404(UserProfile, user=request.user)
+    wishlist = Wishlist.objects.filter(user_profile=user)
+
+    template = 'wishlist/wishlist.html'
+
+    context = {
+        'wishlist': wishlist
+    }
+
+    return render(request, template, context)
