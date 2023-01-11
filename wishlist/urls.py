f968ca10cc9890a20c0070ffcaebd03d611ddab4
JoyZadan <joyzadan.learning@gmail.com>
2023-01-11 17:04:52 +0000

    Added link to wishlist from a user's profile

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('add_to_wishlist/<int:product_id>/', views.add_to_wishlist,
          name='add_to_wishlist'),
     path('remove_from_wishlist/<int:product_id>/', views.remove_from_wishlist,
-         name='remove_from_wishlist')
+         name='remove_from_wishlist'),
 ]
