47dc8ca6db59801cc57a39b00aa942c7e851a442
JoyZadan <joyzadan.learning@gmail.com>
2023-01-19 23:42:42 +0000

    Completed adding meta tags to all products and brands pages

--- products/urls.py
+++ products/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
     path('delete/<int:product_id>/', views.delete_product,
          name='delete_product'),
-    path('brands', views.all_brands, name='brands'),
+    path('brands/', views.all_brands, name='brands'),
     path('brand_detail/<int:brand_id>',
          views.brand_detail, name='brand_detail'),
     path('add_brand/', views.add_brand, name='add_brand'),
