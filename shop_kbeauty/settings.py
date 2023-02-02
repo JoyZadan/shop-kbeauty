3d349158d556387cd9c2038827304c2dd9b027ef
JoyZadan <joyzadan.learning@gmail.com>
2023-02-02 18:08:04 +0000

    Set debug=true to find possible cause of the stray 500 error

--- shop_kbeauty/settings.py
+++ shop_kbeauty/settings.py
@@ -30,14 +30,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
-# DEBUG = True
+# DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = ['shop-k-beauty-django-joy-zadan.herokuapp.com', 'localhost']
 
 
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
